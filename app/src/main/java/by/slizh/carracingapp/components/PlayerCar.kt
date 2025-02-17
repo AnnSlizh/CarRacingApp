970441e39b09296ded01cb1856e9b280c06098fb
Ann Slizh <slizg2002004@gmail.com>
2025-02-17 13:14:59 +0300

    Add Enum for the position of the road strip

--- app/src/main/java/by/slizh/carracingapp/components/PlayerCar.kt
+++ app/src/main/java/by/slizh/carracingapp/components/PlayerCar.kt
@@ -8,17 +8,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import by.slizh.carracingapp.R
 
+enum class Lane(val position: Int, val offset: Dp) {
+    LEFT(0, (-100).dp),
+    CENTER(1, 0.dp),
+    RIGHT(2, 100.dp);
+
+    companion object {
+        fun fromPosition(position: Int): Lane {
+            return entries.find { it.position == position } ?: CENTER
+        }
+    }
+}
+
 @Composable
 fun PlayerCar(lanePosition: Int, modifier: Modifier = Modifier) {
-    val laneOffset = when (lanePosition) {
-        0 -> (-100).dp
-        1 -> 0.dp
-        2 -> 100.dp
-        else -> 0.dp
-    }
+    val laneOffset = Lane.fromPosition(lanePosition).offset
 
     Image(
         painter = painterResource(id = R.drawable.player_car),
