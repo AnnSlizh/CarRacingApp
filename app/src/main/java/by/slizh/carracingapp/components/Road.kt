3498e5f27a1d280a9bfba97a8fb8c248254b7d5b
Ann Slizh <slizg2002004@gmail.com>
2025-02-19 15:46:14 +0300

    Update Player car, Police car and Road

--- app/src/main/java/by/slizh/carracingapp/components/Road.kt
+++ app/src/main/java/by/slizh/carracingapp/components/Road.kt
@@ -9,64 +9,61 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun Road() {
-    val lineWidth = 10.dp
     val density = LocalDensity.current
-    val lineWidthPx = with(density) { lineWidth.toPx() }
-    val offsetY = rememberRoadSpeed()
+    val screenWidth = with(density) { LocalConfiguration.current.screenWidthDp.dp.toPx() }
+    val screenHeight = with(density) { LocalConfiguration.current.screenHeightDp.dp.toPx() }
+    val roadWidth = screenWidth * 0.9f
+    val roadStartX = (screenWidth - roadWidth) / 2
+    val laneWidth = roadWidth / 3
+
+    val lineWidth = 10f
+    val lineHeight = 80f
+    val lineSpacing = 30f
+    val totalLineOffset = lineHeight + lineSpacing
+
+    val offsetY = rememberInfiniteTransition(label = "").animateFloat(
+        initialValue = 0f,
+        targetValue = totalLineOffset,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 500,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
 
     Canvas(modifier = Modifier.fillMaxSize()) {
-        val roadWidth = size.width * 0.9f
-        val roadStartX = (size.width - roadWidth) / 2
-        val laneWidth = roadWidth / 3
-
         drawRect(
             color = Color.DarkGray,
             topLeft = Offset(roadStartX, 0f),
-            size = Size(roadWidth, size.height)
+            size = Size(roadWidth, screenHeight)
         )
 
         for (i in 1..2) {
-            val lineX = roadStartX + laneWidth * i - lineWidthPx / 2
-            var startY = offsetY - with(density) { (80.dp + 30.dp).toPx() }
+            val lineX = roadStartX + laneWidth * i - lineWidth / 2
+            var startY = offsetY.value - totalLineOffset
 
-            while (startY < size.height) {
+            while (startY < screenHeight) {
                 drawRect(
                     color = Color.White,
                     topLeft = Offset(lineX, startY),
-                    size = Size(lineWidthPx, with(density) { 80.dp.toPx() })
+                    size = Size(lineWidth, lineHeight)
                 )
-                startY += with(density) { (80.dp + 30.dp).toPx() }
+                startY += totalLineOffset
             }
         }
     }
 }
 
-@Composable
-fun rememberRoadSpeed(): Float {
-    val lineHeight = 80.dp
-    val lineSpacing = 30.dp
-    val density = LocalDensity.current
-    val totalLineOffset = with(density) { (lineHeight + lineSpacing).toPx() }
 
-    val transition = rememberInfiniteTransition(label = "")
-    val offsetY by transition.animateFloat(
-        initialValue = 0f,
-        targetValue = totalLineOffset,
-        animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = 500, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
-        ), label = ""
-    )
-
-    return offsetY
-}
